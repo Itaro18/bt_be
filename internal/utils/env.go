6f91b834c97a9ae050169519ab7dcd2ffa3e8e4d
Om <om@Oms-MacBook-Pro.local>
2025-07-16 03:09:09 +0530

    deployment changes

--- internal/utils/env.go
+++ internal/utils/env.go
@@ -3,17 +3,19 @@ package utils
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	err := godotenv.Load("../../.env") // Adjust path as needed
-	if err != nil {
-		log.Fatalf("❌ Failed to load .env: %v", err)
+	if os.Getenv("ENV") != "production" {
+		err := godotenv.Load("../../.env") // Adjust path as needed
+		if err != nil {
+			log.Fatalf("❌ Failed to load .env: %v", err)
+		}
 	}
 }
-
 
 func SetJSONContentType(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
